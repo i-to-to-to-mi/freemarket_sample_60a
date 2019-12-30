a3cd1fdd271c863e4037e9dc50c8983457b2406d
Toshio Kaneda <toshiokaneda@ToshionoMacBook-puro.local>
2019-12-30 17:00:17 +0900

    Deviseの作成及びUserのモデルを作成済み

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname,
+      :first_name, :first_name_kana, 
+      :last_name, :last_name_kana, 
+      :birth_year, :birth_month, :birth_day, 
+      :introduction, 
+      :avator])
+  end
+
 
   def production?
     Rails.env.production?
