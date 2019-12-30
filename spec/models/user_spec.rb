7af9bc7138871f7caa521e278171a8df1cdbf74f
Toshio Kaneda <toshiokaneda@ToshionoMacBook-puro.local>
2019-12-30 19:15:46 +0900

    rspect 4 tests done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
     it "email purposely violation" do
-      user = User.new(email: "abcdefg12345")
+      user = build(:user, email: "asd")
       user.valid?
-      expect(user.errors[:email]).to include("email should contains @")
+      expect(user.errors[:email]).to include("is invalid")
     end
+
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "multiple email" do
+      user = create(:user)
+      bad_user = build(:user, email: user.email)
+      bad_user.valid?
+      expect(bad_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "proper email registration" do
+      user = create(:user)
+      good_another = build(:user, email: "kaneda-san@gmail.com")
+      good_another.valid?
+      expect(good_another).to be_valid
+    end
+
   end
 end
