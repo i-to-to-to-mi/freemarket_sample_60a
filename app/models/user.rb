029e69b81ae9210ead151cb7b5d1f5d37f7873e6
i-to-to-to-mi <individual080@gmail.com>
2020-01-05 20:07:41 +0900

    バリデーション
    バリデーション記述
    コメントアウト
    
    ビューの誤記載を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   mount_uploader :avatar, ImageUploader
 
-  has_many :sns_credentials
+  # バリデーション
+  # validates :nickname, :firstname, :lastname, :birthday, presence: true
+  # validates :password, presence: true, length: { minimum: 7 }, 
+  #           # 英数字のみ可
+  #           format: { with: /\A[a-z0-9]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
+  # validates :email, presence: true, 
+  #           # 重複不可
+  #           uniqueness: { case_sensitive: false }, 
+  #           # 英数字のみ可,@を挟んだemailの形になっているか
+  #           format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
+  # validates :kana_firstname, :kana_lastname, presence: true, 
+  #           # カナのみ可
+  #           format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
+
+  # has_one :address
+  # has_many :sns_credentials
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
@@ -22,5 +37,4 @@ class User < ApplicationRecord
     end
     { user: user, sns: sns }
   end
-
 end
