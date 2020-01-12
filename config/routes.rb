d2ebf87fbcd1a1af21e42e4a6b56b46ff57bd6aa
i-to-to-to-mi <57339536+i-to-to-to-mi@users.noreply.github.com>
2020-01-12 15:51:27 +0900

    Revert "Revert "[LGTM]SNS認証実装""

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-
-  get 'users/show'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+devise_scope :user do
+  get 'addresses', to: 'users/registrations#new_address'
+  post 'addresses', to: 'users/registrations#create_address'
+end
   root "items#index"
-  resources :users, :only => [:show, :edit, :update]
-  resources :addresses, :only => [:new, :create]
-  
+  get 'users/show'
+  resources :users, only: [:index,:new, :show, :edit, :update]
+  resources :addresses, only: [:new, :create]
 end
--- db/migrate/20200104105825_change_datatype_prefectures_of_addresses.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypePrefecturesOfAddresses < ActiveRecord::Migration[5.2]
-  def change
-    change_column :addresses, :Prefectures, :integer
-  end
-end
