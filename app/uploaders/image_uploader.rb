57637e19e0e851b2d00dfa8e55c6b2a95308eff4
i-to-to-to-mi <individual080@gmail.com>
2020-02-06 23:33:22 +0900

    fix_a_little
    指摘されそうな箇所コメント入れました

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  #ほかコメントアウトは初期設定のため残してあります。
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick   #コメントアウトを外す
@@ -6,7 +7,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
   # storage :fog
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
