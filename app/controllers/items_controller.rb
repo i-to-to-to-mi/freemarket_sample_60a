a6ff042fa25b9fcf1008b1d899cf6bc30cf946f8
sayukino <lyulyuki@yahoo.co.jp>
2020-02-08 14:56:20 +0900

    商品出品保存できるようになった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-<<<<<<< HEAD
-=======
 
->>>>>>> 7eb51ea47d00021384bc2b2db0ac1e56a3299e7b
   def index
     @user = User.find_by(params[:user_id])
-    @items= Item.includes(:images).order('created_at DESC')
-    
+    @items= Item.includes(:images).order('created_at DESC')  
   end
 
   def new
@@ -20,10 +16,6 @@ class ItemsController < ApplicationController
     @item.margin = @item.price*0.1
     @item.profit = @item.price*0.9
     @item.seller_id = current_user.id
-<<<<<<< HEAD
-
-=======
->>>>>>> 7eb51ea47d00021384bc2b2db0ac1e56a3299e7b
     if @item.save!
       redirect_to root_path, notice: '出品完了しました'
     else
@@ -55,8 +47,7 @@ private
   def total_item_info
     item_params.merge(@item.set_extra_information)
   end
+end
 
 
 
-
-  
