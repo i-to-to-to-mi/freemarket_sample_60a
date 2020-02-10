c91ece41d5923297da23c3f3127fc40034ba977f
i-to-to-to-mi <57339536+i-to-to-to-mi@users.noreply.github.com>
2020-02-10 14:08:55 +0900

    fix_items-controller
    git rebate時、コンフリクト解消するために比較し記述忘れたものです。失礼しました。newとcreateの重複部分を削除致しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,20 +15,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  
-  def new
-    @item = Item.new
-  end
-  
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path, notice: '住所を登録しました'
-    else
-      render :new
-    end
-  end
-
   def show
     @user = User.new
     @item = Item.find(1)
