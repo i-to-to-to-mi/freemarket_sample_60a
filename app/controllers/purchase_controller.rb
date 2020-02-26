94b1bc6a9c16531779751f46be121ef17fd7a4ab
saito yuko <y_s0617@yahoo.co.jp>
2020-02-26 21:57:51 +0900

    登録したカードで購入機能

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,31 +1,44 @@
 class PurchaseController < ApplicationController
-  def show
-  end
-
   require 'payjp'
 
-  # def index
-  #   card = Card.where(user_id: current_user.id).first
-  #   #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
-  #   if card.blank?
-  #     #登録された情報がない場合にカード登録画面に移動
-  #     redirect_to controller: "card", action: "new"
-  #   else
-  #     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-  #     #保管した顧客IDでpayjpから情報取得
-  #     customer = Payjp::Customer.retrieve(card.customer_id)
-  #     #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-  #     @default_card_information = customer.cards.retrieve(card.card_id)
-  #   end
-  # end
+  def show
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = "sk_test_a0029dc5466705b77c5d7bab"
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  
+  def index
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = "sk_test_a0029dc5466705b77c5d7bab"
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
 
   def pay
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = "sk_test_a0029dc5466705b77c5d7bab"
     Payjp::Charge.create(
-    amount: 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
-    card: params['payjp-token'], # フォームを送信すると生成されるトークン
-    # customer: card.customer_id, #顧客ID
+    amount: 6000, #支払金額を入力（itemテーブル等に紐づけても良い）
+    # card: params['payjp-token'], # フォームを送信すると生成されるトークン
+    customer: card.customer_id, #顧客ID
     currency: 'jpy', #日本円
   )
   redirect_to action: 'done' #完了画面に移動
