b8445aac6468c056231eec3c2693f4b49fa3c711
TOSHIOKANEDA <bhoosama@hotmail.com>
2020-03-01 12:26:04 +0900

    register

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -15,7 +15,7 @@ class CardController < ApplicationController
       customer = Payjp::Customer.create(card: params['payjp-token'])
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
         if @card.save
-          redirect_to action: "edit"
+          redirect_to edit_card_index
         else
           redirect_to action: "pay"
           notice[:delete] = "なんかちげー"
