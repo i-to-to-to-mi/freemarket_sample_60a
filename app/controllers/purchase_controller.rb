0f85289b8cf084706161f6d303afb5e745b906cc
saito yuko <y_s0617@yahoo.co.jp>
2020-03-03 21:53:52 +0900

    商品購入画面に、itemテーブルよりデータを反映

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,6 +3,8 @@ class PurchaseController < ApplicationController
 
   def show
     card = Card.where(user_id: current_user.id).first
+    
+    @item= Item.first
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
