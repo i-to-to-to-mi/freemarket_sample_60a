0635fdb72d0545c762fa1969490b3b57ef6658ac
i-to-to-to-mi <individual080@gmail.com>
2020-03-04 01:16:31 +0900

    update

--- db/migrate/20200224101614_remove_author_from_items.rb
+++ db/migrate/20200224101614_remove_author_from_items.rb
@@ -1,6 +1,6 @@
 class RemoveAuthorFromItems < ActiveRecord::Migration[5.2]
   def change
     remove_column :items, :category, :VARCHA
-    add_reference :items,:category_id,foreign_key: true
+    add_reference :items,:category,foreign_key: true
   end
 end
