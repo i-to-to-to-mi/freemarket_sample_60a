d9fdab9993de3fdc1910c35c1bbe5c661c646653
sasashiro <s.zashiro@gmail.com>
2020-03-10 14:07:02 +0900

    Summary

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,8 +13,19 @@ class SearchesController < ApplicationController
   end
 
   private
-  def set_ransack
-    @q = Item.ransack(params[:q])
-  end
+    def detail_search_params
+      params.require(:q)
+      .permit(:name_cont,
+              :price_gteq, 
+              :price_lteq, 
+              :sorts,
+              {state_in: []}, 
+              {fee_payer_in: []}, 
+              {status_in: []})
+    end
+
+    def set_ransack
+      @q = Item.ransack(params[:q])
+    end
   
 end
