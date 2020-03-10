960c340391dcb8751dbe0096c88f26b2fd36ed6a
TOSHIOKANEDA <bhoosama@hotmail.com>
2020-03-11 00:43:08 +0900

    showに入る前にif文で分岐させた

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,19 +1,19 @@
 class PurchaseController < ApplicationController
   require 'payjp'
   before_action :set_item, only: [:show, :pay, :done]
-  before_action :set_card, only: [:show, :pay, :done]
+  before_action :set_card, only: [:pay, :done]
 
-  def show
-    # @user = User.find(current_user.id)
-    if @card.blank?
-      #登録された情報がない場合にカード登録画面に移動
-      redirect_to controller: "card", action: "new"
+  def show    
+    if current_user.card_id.present?
+        @card = Card.where(buyer_id: current_user.id).first
+        Payjp.api_key = 'sk_test_a0029dc5466705b77c5d7bab'
+        #保管した顧客IDでpayjpから情報取得
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+        @default_card_information = customer.cards.retrieve(@card.card_id)
     else
-      Payjp.api_key = 'sk_test_a0029dc5466705b77c5d7bab'
-      #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(@card.card_id)
+      redirect_back(fallback_location: root_path)
+      flash[:notice] = 'まずはクレジットカードの登録をしてください'
     end
   end
 
@@ -26,8 +26,8 @@ class PurchaseController < ApplicationController
   )
   redirect_to done_purchase_index_path(id:@item.id) #完了画面に移動
   end
+
   def done
-  # @user = User.find(current_user.id)
   customer = Payjp::Customer.retrieve(@card.customer_id)
   @default_card_information = customer.cards.retrieve(@card.card_id)
   @item_buyer= Item.find(params[:id])
@@ -35,7 +35,7 @@ class PurchaseController < ApplicationController
     if @item_buyer.save
     else
       redirect_to parchase_path
-      notice[:delete] = "購入できませんでした"
+      flash[:notice] = '編集が完了しました'
     end
   end
 
