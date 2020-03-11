ec6411029b3ffe9ef137287376ddecc37140d8e9
sasashiro <s.zashiro@gmail.com>
2020-03-11 12:02:42 +0900

    Commit

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,31 +2,31 @@ class SearchesController < ApplicationController
   before_action :set_ransack
   
   def index
-    @ladies = Item.search(params[:search]).limit(132)
+    @items = Item.search(params[:search]).limit(132)
     @search = params[:search]
   end
 
   def detail_search
     @search = params[:q][:name_cont]
     @search_item = Item.ransack(params[:q]) 
-    @ladies = @search_item.result.page(params[:page])
+    @items = @search_item.result.page(params[:page])
   end
 
   private
-    def detail_search_params
-      params.require(:q)
-      .permit(:name_cont,
-              :price_gteq, 
-              :price_lteq, 
-              :sorts,
-              # {state_in: []}, 
-              # {fee_payer_in: []}, 
-              # {status_in: []}
-            )
-    end
+  def set_ransack
+    @q = Item.ransack(params[:q])
+  end
 
-    def set_ransack
-      @q = Item.ransack(params[:q])
-    end
+  def detail_search_params
+    params.require(:q)
+    .permit(:name_or_category_name_or_brand_name_cont,
+            {category_id_in: []}, 
+            :brand_name_cont, 
+            :price_gteq, 
+            :price_lteq, 
+            {condition_in: []}, 
+            {shipping_fee_in: []}, 
+            {status_in: []})
+  end
   
 end
