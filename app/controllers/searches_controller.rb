ab4fe7302267d5e063895a1e45ed0b4abc3e48ae
i-to-to-to-mi <individual080@gmail.com>
2020-03-12 23:51:11 +0900

    記述なくても画像でた

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,5 @@ class SearchesController < ApplicationController
   def index
     @search = Item.ransack(params[:q])
     @items = @search.result
-    @images = Image.where(item_id:1..999)
   end
 end
