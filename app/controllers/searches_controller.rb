a037d26c48a46be37fd5da7c1c245eb5e19627dc
i-to-to-to-mi <individual080@gmail.com>
2020-03-13 01:08:06 +0900

    検索L2エラーでないとこだけ実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,30 @@
+# class SearchesController < ApplicationController
+  
+#   def index
+#     @items = Item.search(params[:search]).limit(132)
+#     @search = params[:search]
+#     @category_parent_array = Category.where(ancestry: nil).pluck(:name)
+#   end
+#   def category_children
+#     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+#   end
+
+#   def category_grandchildren
+#     @category_grandchildren = Category.find("#{params[:child_id]}").children
+#   end
+# end
 class SearchesController < ApplicationController
 
   def index
-    @search = Item.ransack(params[:q])
+    @search = Item.ransack(params[:q]) 
     @items = @search.result
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name)
   end
+  def category_children
+        @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+      end
+    
+      def category_grandchildren
+        @category_grandchildren = Category.find("#{params[:child_id]}").children
+      end
 end
