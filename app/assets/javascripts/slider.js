54bfbddd818c42f4e3251188f892e2937653267d
i-to-to-to-mi <individual080@gmail.com>
2020-03-19 22:53:44 +0900

    flash-fix
    自動的に消えるようにjsを導入
    slider機能しかつかってないitem.jsをsliderのみ分離し削除する

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.liked-items__sliders').slick({
+    slidesToShow: 6,
+    arrow: true,
+    slidesToScroll: 6,
+    variableWidth: true,
+    centerMode: false,
+    centerPadding: '0%',
+  });
+});
